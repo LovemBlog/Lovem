d6042faa398ca411d594a23555be08796e22b243
luchengwei <luchengwei@bubi.cn>
2019-02-14 20:05:48 +0800

    修改部分样式，开始文章目录模块的开发

--- footer.php
+++ footer.php
@@ -7,6 +7,12 @@
 <footer id="footer" role="contentinfo">
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
     <?php _e('由 <a href="http://www.typecho.org">Typecho</a> 强力驱动'); ?>.
+    <?php if($this->user->hasLogin()): ?>
+        <a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a>
+        <a href="<?php $this->options->logoutUrl(); ?>"><?php _e('退出'); ?></a>
+    <?php else: ?>
+        <a href="<?php $this->options->adminUrl('login.php'); ?>"><?php _e('登录'); ?></a>
+    <?php endif; ?>
 </footer><!-- end #footer -->
 <?php $this->footer(); ?>
 <script src="//cdn.bootcss.com/highlight.js/9.12.0/highlight.min.js"></script>
