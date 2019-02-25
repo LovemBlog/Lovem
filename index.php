4f38d3493a9f27cfc9ba242d14b30d4571f41ff1
luchengwei <luchengwei@bubi.cn>
2019-02-25 15:02:24 +0800

    save

--- index.php
+++ index.php
@@ -22,7 +22,6 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 				<li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
 				<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
 			</ul>
-			<!-- index-menu -->
             <div class="post-content" itemprop="articleBody">
     			<?php $this->content('- 阅读剩余部分 -'); ?>
             </div>
