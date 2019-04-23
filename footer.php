b1cf089b7156cd27cb463fe9bd05200cb0e7c3cc
luchengwei <luchengwei@bubi.cn>
2019-04-23 14:20:22 +0800

    add icp

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 </div><!-- end #body -->
 
 <footer id="footer" role="contentinfo">
-    &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
+    &copy; <?php echo date('Y'); ?> 鲁ICP备18003483号 <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
     <?php _e('由 <a href="http://www.typecho.org">Typecho</a> 强力驱动'); ?>.
     <?php if($this->user->hasLogin()): ?>
         <a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a>
