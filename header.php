f3b69feb00c90f89c61d2cd789228364cd30717e
goodlcw1 <goodlcw1@163.com>
2021-03-19 16:54:53 +0800

    feat: 1. 增加meta做百度爬虫验证；2. 更改字体顺序，提升Mac浏览体验

--- header.php
+++ header.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title><?php $this->archiveTitle(array(
+    <meta name="baidu-site-verification" content="code-MRd5G8U3Ai" />
+ <title><?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
             'search'    =>  _t('包含关键字 %s 的文章'),
             'tag'       =>  _t('标签 %s 下的文章'),
